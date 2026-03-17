ce863a56fbec600be8a5d8055ad7f51e76a9bdab
Eduard289 <78046334+Eduard289@users.noreply.github.com>
2026-03-17 20:22:49 +0100

    Create visualizer.js

---
+++ visualizer.js
@@ -0,0 +1,38 @@
+// Este script simula un "caldo de cultivo" visual
+const canvas = document.getElementById('simCanvas');
+const ctx = canvas.getContext('2d');
+
+let particles = [];
+
+class Particle {
+    constructor(color) {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.vx = (Math.random() - 0.5) * 2;
+        this.vy = (Math.random() - 0.5) * 2;
+        this.color = color;
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.x < 0 || this.x > canvas.width) this.vx *= -1;
+        if (this.y < 0 || this.y > canvas.height) this.vy *= -1;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 3, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(p => {
+        p.update();
+        p.draw();
+    });
+    requestAnimationFrame(animate);
+}
